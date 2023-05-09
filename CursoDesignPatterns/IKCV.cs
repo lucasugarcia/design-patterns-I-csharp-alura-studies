dd8dd74b1a2fff5ae51607ed2352432c2dd29d5c
Lucas Garcia <lucasugarcia@gmail.com>
2023-05-09 06:05:41 -0300

    Implementação do design pattern template method, diminuindo a repetição de código dos ifs dos novos impostos e facilitando futuras alterações

---
+++ CursoDesignPatterns/IKCV.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+    public class IKCV : TemplateDeImpostoCondicional
+    {
+        public override bool DeveUsarMaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor > 500 && temItemMaiorQue100ReaisNo(orcamento);
+        }
+
+        public override double MaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.1;
+        }
+
+        public override double MinimaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.06;
+        }
+
+        private bool temItemMaiorQue100ReaisNo(Orcamento orcamento)
+        {
+            return orcamento.Itens.Any(i => i.Valor > 100);
+        }
+    }
+}
